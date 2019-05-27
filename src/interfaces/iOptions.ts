d680e3ef618ab19e3782d99da041278c2586b8f4
Artyom Popov <artyom.popov@bytebrand.net>
2019-05-27 17:11:23 +0300

    rewrite to typesctipt

---
+++ src/interfaces/iOptions.ts
@@ -0,0 +1,10 @@
+export interface iOptions {
+  src: string;
+  dest: string;
+  filePath?: string;
+  name?: string;
+  absolute?: boolean;
+  overwrite?: boolean;
+  ignore?: boolean;
+  compression?: any;
+}
